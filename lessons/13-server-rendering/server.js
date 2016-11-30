d7e2b4de0dba1b93b10adf119f440883c43b9e5e
Andrey Yaroshenko <kradio3@gmail.com>
2016-11-30 14:29:44 +0300

    feat(Server): render on server

--- lessons/13-server-rendering/server.js
+++ lessons/13-server-rendering/server.js
@@ -2,6 +2,13 @@ var express = require('express')
 var path = require('path')
 var compression = require('compression')
 
+import React from 'react'
+// we'll use this to render our app to an html string
+import { renderToString } from 'react-dom/server'
+// and these to match the url to routes and then render
+import { match, RouterContext } from 'react-router'
+import routes from './modules/routes'
+
 var app = express()
 
 app.use(compression())
@@ -10,11 +17,39 @@ app.use(compression())
 app.use(express.static(path.join(__dirname, 'public')))
 
 // send all requests to index.html so browserHistory works
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+
+app.get('*', (req, res) => {
+  console.log('app get')
+  // match the routes to the url
+  match({ routes: routes, location: req.url }, (err, redirect, props) => {
+    // `RouterContext` is what the `Router` renders. `Router` keeps these
+    // `props` in its state as it listens to `browserHistory`. But on the
+    // server our app is stateless, so we need to use `match` to
+    // get these props before rendering.
+    const appHtml = renderToString(<RouterContext {...props}/>)
+    console.log(appHtml)
+
+    // dump the HTML into a template, lots of ways to do this, but none are
+    // really influenced by React Router, so we're just using a little
+    // function, `renderPage`
+    res.send(renderPage(appHtml))
+  })
 })
+
+function renderPage(appHtml) {
+  return `
+    <!doctype html public="storage">
+    <html>
+    <meta charset=utf-8/>
+    <title>My First React Router App 2</title>
+    <link rel=stylesheet href=/index.css>
+    <div id=app>${appHtml}</div>
+    <script src="/bundle.js"></script>
+   `
+}
 
 var PORT = process.env.PORT || 8080
 app.listen(PORT, function() {
+  console.log('test changes')
   console.log('Production Express server running at localhost:' + PORT)
 })
